e28f112e42431de52be0aad61bbaaf7bf809914b
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-14 14:11:36 -0400

    added more content inside UPDATE_POSTS

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -2,3 +2,4 @@ export const UPDATE_POST = "UPDATE_POST";
 export const ADD_POST = "ADD_POST";
 export const REMOVE_POST = "REMOVE_POST";
 export const SET_CURRENT_POST = "SET_CURRENT_POST";
+export const LOADING = "LOADING";
