fb6e1459d9579402d3c55053e37275e4137e5852
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-14 13:36:22 -0400

    added switch cases inside global state and added blog context function

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,8 +1,27 @@
-import React, { useReducer } from "react";
+import React, { createContext, useReducer, useContext } from "react";
 // Don't forget to import all of your actions!
+
+const BlogContext = createContext({
+  id: "",
+  name: ""
+})
 
 const reducer = (state, action) => {
   switch (action.type) {
+  case "UPDATE_POSTS":
+    return 
+  case "ADD_POST":
+    return
+  case "REMOVE_POST":
+    return
+  case "SET_CURRENT_POST":
+    return {
+      ...state,
+      currentPost: action.post,
+      loading: false
+    };
+  case "LOADING":
+    return
   default:
     return state;
   }
@@ -14,6 +33,8 @@ const StoreProvider = ({ value = [], ...props }) => {
   return "PROVIDER ELEMENT HERE";
 };
 
-const useStoreContext = () => {};
+const useStoreContext = () => {
+  return useContext(BlogContext);
+};
 
 export { StoreProvider, useStoreContext };
